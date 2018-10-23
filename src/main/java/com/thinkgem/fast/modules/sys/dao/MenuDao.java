99fba419726ee169032ba3f6fa30e4f531b499e0
renshuo <renshuo@bitnei.cn>
2018-10-23 18:46:58 +0800

    初始化代码

---
+++ src/main/java/com/thinkgem/fast/modules/sys/dao/MenuDao.java
@@ -0,0 +1,25 @@
+package com.thinkgem.fast.modules.sys.dao;
+
+import java.util.List;
+
+import com.thinkgem.fast.common.persistence.CrudDao;
+import com.thinkgem.fast.common.persistence.annotation.MyBatisDao;
+import com.thinkgem.fast.modules.sys.entity.Menu;
+
+/**
+ * 菜单DAO接口
+ * @author renshuo
+ * @version 2014-05-16
+ */
+@MyBatisDao
+public interface MenuDao extends CrudDao<Menu> {
+
+	public List<Menu> findByParentIdsLike(Menu menu);
+
+	public List<Menu> findByUserId(Menu menu);
+	
+	public int updateParentIds(Menu menu);
+	
+	public int updateSort(Menu menu);
+	
+}
